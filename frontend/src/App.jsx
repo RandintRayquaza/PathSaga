8c373dd925060141aaabcdf0ba5dab1797e8608e
Aryan <roaringmoon07@gmail.com>
2026-02-24 16:07:12 +0530

    wroking on frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import AppRouter from './routes/AppRouter'
-import Profile from './pages/Profile'
-  
+
 const App = () => {
   return (
-   <Profile />
+  <AppRouter/>
   )
 }
 
--- frontend/src/routes/AppRouter.jsx
+++ frontend/src/routes/AppRouter.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import{Routes,Route} from 'react-router-dom'
 import Home from '../pages/Home'
+import Profile from '../pages/Profile'
 const AppRouter = () => {
   return (
     <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
     </Routes>
   )
 }
