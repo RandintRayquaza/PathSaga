8dbbf78d60d1baa8fcbee6dcea73d4f366dc4367
Aryan <roaringmoon07@gmail.com>
2026-04-23 08:54:24 +0000

    added vercel SpeedInsights and Analytics

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,8 @@
 
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import { store } from './redux/store';
 import App from './App';
 import './index.css';
@@ -9,5 +11,7 @@ import './i18n';
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
+    <Analytics />
+    <SpeedInsights />
   </Provider>
 );
